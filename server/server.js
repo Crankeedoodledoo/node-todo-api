61b44ad1bf57c574ff78b06a0ef7ba6fc5a9fa38
Mina Puzovic <mina.puzovic@outlook.com>
2017-05-03 12:54:48 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.delete('/todos/:id', (req,res) => {
       return res.status(404).send();
     }
     //if doc, send doc back with 200
-    res.send(todo);
+    res.send({todo});
 
     //console.error
       //400 with empty body
